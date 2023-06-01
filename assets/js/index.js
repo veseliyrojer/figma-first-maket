fbbe80406c20e0b5d64251d74d126d5d555d55ef
veseliyrojer <veseliyrojer64@gmail.com>
2023-06-01 02:45:53 +0200

    add two sections

---
+++ assets/js/index.js
@@ -0,0 +1,6 @@
+const btn = document.getElementById('burger')
+const navList = document.getElementById('nav-list')
+
+btn.addEventListener('click', () => {
+    navList.classList.toggle('nav-list-visible')
+})
